5d0e836d3e058f64f5259aeb9c56a323ddc1ce10
Will-Eves <63985486+Will-Eves@users.noreply.github.com>
2022-03-18 23:30:30 -0400

    Update OpenProfile.h

--- OpenProfile.h
+++ OpenProfile.h
@@ -12,13 +12,13 @@ namespace OpenProfile{
     float StampEvent(std::string name){
         auto current = std::chrono::high_resolution_clock::now();
         auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
+        this->events[name] = current;
         return delta/1000.0f;
     }
     
     float SnapEvent(std::string name){
         auto current = std::chrono::high_resolution_clock::now();
         auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
-        this->events[name] = current;
         return delta/1000.0f;
     }
 
