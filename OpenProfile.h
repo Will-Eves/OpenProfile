b396d0b2133e5fdbd91d0c08ad2f613fb4b25e3d
Will-Eves <63985486+Will-Eves@users.noreply.github.com>
2022-03-18 23:25:32 -0400

    Update OpenProfile.h

--- OpenProfile.h
+++ OpenProfile.h
@@ -6,19 +6,25 @@ namespace OpenProfile{
     std::map<std::string, std::chrono::_V2::system_clock::time_point> events;
 
     void StartEvent(std::string name){
-        events[name] = std::chrono::high_resolution_clock::now();
+        this->events[name] = std::chrono::high_resolution_clock::now();
     }
 
     float StampEvent(std::string name){
         auto current = std::chrono::high_resolution_clock::now();
-        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-events[name]).count();
-        events[name] = current;
+        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
+        return delta/1000.0f;
+    }
+    
+    float SnapEvent(std::string name){
+        auto current = std::chrono::high_resolution_clock::now();
+        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
+        this->events[name] = current;
         return delta/1000.0f;
     }
 
     float EndEvent(std::string name){
         auto current = std::chrono::high_resolution_clock::now();
-        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-events[name]).count();
+        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
         return delta/1000.0f;
     }
 }
