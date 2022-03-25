ea2e468b65effe64289eb48c18a335930436a903
Will-Eves <63985486+Will-Eves@users.noreply.github.com>
2022-03-25 11:28:02 -0400

    Update OpenProfile.h

--- OpenProfile.h
+++ OpenProfile.h
@@ -6,25 +6,25 @@ namespace OpenProfile{
     std::map<std::string, std::chrono::_V2::system_clock::time_point> events;
 
     void StartEvent(std::string name){
-        this->events[name] = std::chrono::high_resolution_clock::now();
+        events[name] = std::chrono::high_resolution_clock::now();
     }
 
     float StampEvent(std::string name){
         auto current = std::chrono::high_resolution_clock::now();
-        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
-        this->events[name] = current;
-        return delta/1000.0f;
+        auto delta = std::chrono::duration_cast<std::chrono::nanoseconds>(current-events[name]).count();
+        events[name] = current;
+        return delta/1000000000.0f;
     }
     
     float SnapEvent(std::string name){
         auto current = std::chrono::high_resolution_clock::now();
-        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
-        return delta/1000.0f;
+        auto delta = std::chrono::duration_cast<std::chrono::nanoseconds>(current-events[name]).count();
+        return delta/1000000000.0f;
     }
 
     float EndEvent(std::string name){
         auto current = std::chrono::high_resolution_clock::now();
-        auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(current-this->events[name]).count();
-        return delta/1000.0f;
+        auto delta = std::chrono::duration_cast<std::chrono::nanoseconds>(current-events[name]).count();
+        return delta/1000000000.0f;
     }
 }
